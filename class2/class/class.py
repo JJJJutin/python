f1c0bc6e9253dc28fe52c5b65a54efe0bcb207ad
JJJJutin <justinchiu1130@gmail.com>
2022-09-24 15:30:31 +0800

    class2-class

---
+++ class2/class/class.py
@@ -0,0 +1,29 @@
+"""
+max(1,2,3)
+3
+len("JJJJutin")
+8
+type(12345)
+<class 'int'>
+"""
+"""
+print("處存")
+a = input()
+print("內容")
+print(a)
+print("資料型態")
+print(type(a))
+a = input("此文字為提示文字 請輸入:")
+print(a)
+"""
+"""
+a = input("Please enter your age:")
+print("Your age is " + a + " years old!")
+"""
+"""
+r = input("輸入半徑:")
+r = int(r)
+pi = 3.14
+area = pi * r**2
+print(area)
+"""
