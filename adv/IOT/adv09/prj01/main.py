e39951910da86b89909b42cd5b379e5995973665
JJJJutin <justinchiu1130@gmail.com>
2024-05-05 11:17:12 +0800

    adv07 08

--- adv/IOT/adv09/prj01/main.py
+++ adv/IOT/adv09/prj01/main.py
@@ -1,15 +1,21 @@
 #########################匯入模組#########################
 import mcu
+from machine import Pin, I2C
+import ssd1306
 
 #########################函式與類別定義#########################
+gpio = mcu.gpio()
+i2C = I2C(scl=Pin(gpio.D1), sda=Pin(gpio.D2))
+oled = ssd1306.SSD1306_I2C(128, 64, i2C)
 
 #########################宣告與設定#########################
 wi = mcu.wifi("SingularClass", "Singular#1234")
-
 wi.setup(ap_active=False, sta_active=True)
-
-wi.scan()
-
 if wi.connect():
     print(f"IP={wi.ip}")
-#########################主程式#########################
+#########################主程式########################
+oled.fill(0)
+oled.text(f"{wi.ip}", 0, 0)
+oled.text("Mother", 0, 10)
+oled.text("Fxcker", 0, 20)
+oled.show()
---
+++ adv/IOT/adv09/prj02/main.py
@@ -0,0 +1,27 @@
+#########################匯入模組#########################
+import paho.mqtt.client as mqtt
+import time
+
+
+#########################函式與類別定義#########################
+def on_publish(client, userdata, mid, reason_code, properties):
+    print(f"Message {mid} has been published.")
+
+
+#########################宣告與設定#########################
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+client.on_publish = on_publish
+client.username_pw_set("singular", "Singular#1234")
+client.connect("mqtt.singularinnovation-ai.com", 1883, 60)
+client.loop_start()
+#########################主程式#########################
+while True:
+    msg = input("Enter MQTT Message:")
+    result = client.publish("mdfk", msg)
+    result.wait_for_publish()
+
+    if result.rc == mqtt.MQTT_ERR_SUCCESS:
+        print("Message published successfully")
+    else:
+        print("Failed to publish message")
+    time.sleep(0.1)
